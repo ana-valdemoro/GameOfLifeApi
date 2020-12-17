04d0391ec8f15704adc8f5ad2c77ee4bb83e88b8
Ana Valdemoro Ramirez <avaldemoro@domingoalonsogroup.com>
2020-12-17 16:09:10 +0000

    Trying to resolve errors

--- API/HealthCheck/FileHealthCheck.cs
+++ API/HealthCheck/FileHealthCheck.cs
@@ -8,30 +8,45 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace GameOfLifeApi2.HealthCheck
 {
-    public class FileHealthCheck: IHealthCheck
+    public class FileHealthCheck : IHealthCheck
     {
         public readonly IConfiguration Configuration;
         public readonly string CurrentDirectory;
 
-        public FileHealthCheck(IConfiguration configuration) {
+        public FileHealthCheck(IConfiguration configuration)
+        {
             Configuration = configuration;
             CurrentDirectory = Directory.GetCurrentDirectory();
         }
+
         public Task<HealthCheckResult> CheckHealthAsync(
-            HealthCheckContext context, 
-            CancellationToken cancellationToken = default(CancellationToken)) {
-            return Task.FromResult(HasWritePermissionOnDirectory() ? 
-                HealthCheckResult.Healthy("A healthy result.Folder is accessible to read and write") :
-                HealthCheckResult.Unhealthy("Unhealthy result: Folder doesn't have write permissions"));
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult(HasWritePermissionOnDirectory()
+                ? HealthCheckResult.Healthy("A healthy result.Folder is accessible to read and write")
+                : HealthCheckResult.Unhealthy("Unhealthy result: Folder doesn't have write permissions"));
         }
 
-        public bool HasWritePermissionOnDirectory() {
+        public bool HasWritePermissionOnDirectory()
+        {
             var writeAllow = false;
-            FileInfo fileInfo = new FileInfo(CurrentDirectory);
-            if ((fileInfo.Attributes & FileAttributes.ReadOnly) != FileAttributes.ReadOnly) {
-                writeAllow = true;
+            //FileInfo fileInfo = new FileInfo(CurrentDirectory);
+            //if ((fileInfo.Attributes & FileAttributes.ReadOnly) != FileAttributes.ReadOnly) {
+            //    writeAllow = true;
+            //}
+            DirectoryInfo dInfo = new DirectoryInfo(CurrentDirectory);
+            DirectorySecurity accessControlList = dInfo.GetAccessControl();
+            var accessRules = accessControlList.GetAccessRules(true, true, 
+                typeof(System.Security.Principal.SecurityIdentifier));
+            foreach (FileSystemAccessRule rule in accessRules)
+            {
+                if ((FileSystemRights.Write & rule.FileSystemRights) != FileSystemRights.Write)
+                    continue;
+                if (rule.AccessControlType == AccessControlType.Allow) writeAllow = true;
             }
-            return writeAllow ;
+            return writeAllow;
         }
+        
     }
 }
